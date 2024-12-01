0058972f6fed431a8b9590dad07cc656465a174c
Janboa <132581790+Janboaaa@users.noreply.github.com>
2024-12-01 16:22:31 +0100

    chors: update eslintrc and format

--- Projects/Darts.WebApp/.eslintrc.cjs
+++ Projects/Darts.WebApp/.eslintrc.cjs
@@ -1,48 +1,30 @@
 module.exports = {
-  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
-  // This option interrupts the configuration hierarchy at this file
-  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
   root: true,
 
-  // https://eslint.vuejs.org/user-guide/#how-to-use-a-custom-parser
-  // Must use parserOptions instead of "parser" to allow vue-eslint-parser to keep working
-  // `parser: 'vue-eslint-parser'` is already included with any 'plugin:vue/**' config and should be omitted
   parserOptions: {
     parser: require.resolve('@typescript-eslint/parser'),
-    extraFileExtensions: [ '.vue' ]
+    extraFileExtensions: ['.vue'],
   },
 
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
 
-  // Rules order is important, please avoid shuffling them
   extends: [
     'eslint:recommended', // Grundregeln von ESLint
     'plugin:@typescript-eslint/recommended', // TypeScript-Regeln
     'plugin:vue/vue3-essential', // Vue3 spezifische Regeln
     'prettier', // Deaktiviert alle ESLint-Regeln, die mit Prettier kollidieren
-    'plugin:prettier/recommended' // Aktiviert Prettier als Plugin und Konfiguration
-  ],  
-
-  plugins: [
-    // required to apply rules which need type information
-    '@typescript-eslint',
-
-    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
-    // required to lint *.vue files
-    'vue'
-    
-    // https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
-    // Prettier has not been included as plugin to avoid performance impact
-    // add it as an extension for your IDE
-    
+    'plugin:vue/vue3-recommended', // Falls du Vue 3 nutzt
+    'plugin:prettier/recommended', // Aktiviert Prettier als Plugin und Konfiguration
   ],
 
+  plugins: ['@typescript-eslint', 'vue', 'prettier'],
+
   globals: {
-    ga: 'readonly', // Google Analytics
+    ga: 'readonly',
     cordova: 'readonly',
     __statics: 'readonly',
     __QUASAR_SSR__: 'readonly',
@@ -51,28 +33,24 @@ module.exports = {
     __QUASAR_SSR_PWA__: 'readonly',
     process: 'readonly',
     Capacitor: 'readonly',
-    chrome: 'readonly'
+    chrome: 'readonly',
   },
 
-  // add your custom rules here
-  rules: {    
-    'prettier/prettier': ['error'],
+  rules: {
     '@typescript-eslint/no-explicit-any': 'error',
     'prefer-promise-reject-errors': 'off',
-    
     quotes: ['warn', 'single', { avoidEscape: true }],
-
-    // this rule, if on, would require explicit return type on the `render` function
     '@typescript-eslint/explicit-function-return-type': 'off',
-
-    // in plain CommonJS modules, you can't use `import foo = require('foo')` to pass this rule, so it has to be disabled
     '@typescript-eslint/no-var-requires': 'off',
-
-    // The core 'no-unused-vars' rules (in the eslint:recommended ruleset)
-    // does not work with type definitions
     'no-unused-vars': 'off',
-
-    // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleAttributePerLine: false, // Erlaube mehrere Attribute in einer Zeile
+      },
+    ],
+  },
+};
